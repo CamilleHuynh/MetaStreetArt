1b9c4a9a54621006dc3e7640d8ef65907bbbe826
m21-cerutti <marc.cerutti@outlook.fr>
2023-02-22 16:41:40 +0100

    Script material

--- Smartfox Metavers Client/Assets/Scripts/SO/Stickers.cs
+++ Smartfox Metavers Client/Assets/Scripts/SO/Stickers.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 [CreateAssetMenu(fileName = "StickerDecals", menuName = "ScriptableObjects/Decals/StickerDecalList")]
 public class Stickers : ScriptableObject
 {
@@ -17,4 +21,22 @@ public class Stickers : ScriptableObject
     public Material baseMaterial;
     public DecalProjector decalProjectorPrefab;
     public List<Sticker> stickerList;
+    
+    #if UNITY_EDITOR
+    
+    [ContextMenu("Spawn materials")]
+    public void SpawnMaterialsInFolder()
+    {
+        foreach(Sticker s in stickerList)
+        {
+            if (s.mat == null)
+            {
+                Material material = new Material (Shader.Find("HDRP/Decal"));
+                AssetDatabase.CreateAsset(material, "Assets/Materials/DecalMat/sticker_mat/"+s.Image.name+".mat");
+                s.mat = material;
+                EditorUtility.SetDirty(this);
+            }
+        }
+    }
+    #endif
 }
